7260680df24b4b2d05d5dd2847c3724a21796514
sharath-psh <sharath.psh1@gmail.com>
2020-01-21 10:54:05 +0530

    added img resize

--- 2iv.py
+++ 2iv.py
@@ -21,3 +21,21 @@ for file in os.listdir('.'):
 	# im.show() # uncomment this for displaying the image 
 
 
+mean_width = int(mean_width / num_of_images) 
+mean_height = int(mean_height / num_of_images) 
+
+
+for file in os.listdir('.'): 
+	if file.endswith(".jpg") or file.endswith(".jpeg") or file.endswith("png"): 
+		# opening image using PIL Image 
+		im = Image.open(os.path.join(path, file)) 
+
+		# im.size includes the height and width of image 
+		width, height = im.size 
+		print(width, height) 
+
+		# resizing 
+		imResize = im.resize((mean_width, mean_height), Image.ANTIALIAS) 
+		imResize.save( file, 'JPEG', quality = 95) # setting quality 
+		# printing each resized image name 
+		print(im.filename.split('\\')[-1], " is resized") 
